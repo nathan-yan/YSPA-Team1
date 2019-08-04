9a212fe5c0a83810535002c965864217bdf91b2b
nathan-yan <nathancyan2002@gmail.com>
2019-08-03 20:28:45 -0400

    Team 1, signing off!

---
+++ photos and stuff/blog_post_run.py
@@ -0,0 +1,46 @@
+
+
+    import zstd;d=zstd.decompress;k="""abcdefghijklmnopqrstuvwxyzABCDE
+    FGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()""";c="""aO.cL.aV.dL.bC.c
+    c.ae.dv.ay.aa.aw.dE.b#.aG.aq.at.dd.aa.dF.bi.cg.da.cj.cX.a).c&.cn.a
+    &.aN.bL.c).bt.dw.dJ.av.af.b2.bG.c).b(.b%.aa.aa.a#.aa.di.b1.aa.b0.a
+    a.b9.aa.b^.bM.dB.bL.dI.bg.bC.de.do.dc.a).b(.bl.bh.cC.cz.bR.bS.dM.b
+    q.c8.bu.dr.b^.cD.de.aF.b!.b1.aR.aH.cZ.a#.aC.bz.c*.c6.cq.cY.dm.du.b
+    s.aJ.bC.am.bl.dg.aU.a).b*.c1.co.ao.c*.a9.cz.a2.bx.cr.cv.cm.a@.cO.a
+    t.bl.bJ.dM.cY.ae.af.c2.c^.bI.bO.a7.c).bj.dD.at.dE.a6.dp.c!.aA.b0.b
+    1.bT.aQ.b(.a8.aO.bP.db.bU.a*.b).cm.c4.c@.bT.cT.b#.b2.bh.cx.dM.bu.c
+    %.dM.cE.a7.cA.ak.aa.b4.ce.bJ.aF.dg.b@.dJ.bO.bm.av.ab.aj.c@.cv.c1.b
+    E.b0.c!.dq.aQ.b3.bU.cL.b*.bu.dp.c2.aA.dk.cw.c8.aP.cP.bo.cf.aR.cA.d
+    d.aS.cz.du.cS.c&.at.cw.dA.a6.b@.aJ.dd.bO.b8.bz.b@.dE.ck.bR.cf.a$.c
+    k.a1.cK.au.bD.cv.dh.aX.br.bQ.dr.bQ.a!.cx.bh.dD.aD.be.b*.aZ.bU.df.c
+    c.a@.bY.dL.a5.cU.do.a5.bN.ck.dw.bQ.dL.b@.bX.ds.bn.dk.ba.cE.af.at.b
+    M.dm.aZ.c4.cc.c1.as.do.aw.dz.aF.cD.cg.cf.dx.bZ.cs.ds.bJ.bw.c8.dF.a
+    *.b9.ao.cl.bZ.cL.cX.a9.aZ.bM.c6.aD.bf.bS.ab.aa.b4.aq.dC.bY.a%.do.c
+    1.aC.bA.aa.av.aW.cw.bZ.bE.bo.cd.ct.b$.at.aw.aO.du.au.a3.cK.cA.cq.c
+    ^.cK.b!.aJ.bd.cE.c0.cE.cr.a4.cF.bn.am.bh.cc.c3.aA.dv.cW.bV.c^.dm.b
+    (.dJ.bE.bK.bd----.bx----.--------a---------U.c0.-----cx.dK.as.cl.a
+    x.bZ.bq.cA.c2.----d----j----.a-------M.c----R.b------W.dn.bk.cE.cD
+    .cO.dv.ap.aq.bk-------.a-----2.bD.---c*.c---7.-------bz.c9.aT.bb.b
+    (.cL.b!.bj.c0.b5-----.b(.-------cA---.a@----.----a---u.c6.c2.aw.aE
+    .cq.dy.du.aE.b7.c---B.ak.aM.---------------c----3.---cU.dC.cw.b).d
+    k.aJ.bJ.au.cl.a@.---bX.c8.aD.b-------n.ab.a----p.a---a.ai.cs.c0.cL
+    .da.dm.dz.aH.bJ.b---2.bN----.c-------9.dB.-----------a5.av.a).am.a
+    o.am.ao.cG.dH.aw.---cL.a#--------.---bI.b----Y.a%.---bR.c7.bW.aE.a
+    6.c0.aF.dt.bz.du.bQ.aP.c6.cY.ar.c2.bu.am.bV.a$.b(.cL.a0.c^.ce.ax.b
+    P.cA.ax.aB.c2.dC.b8.b*.cA.cK.ci.a3.bx.b7.aA.cN.cj.dx.c@.aE.bM.a6.a
+    6.bW.c$.a8.cT.cM.aP.bh.db.bu.bR.cM.bi.bA.bR.a*.cd.bC.dv.cr.a%.cK.c
+    e.dl.ca.ax.bl.bs.aC.br.bm.cD.bX.c).bo.cg.dk.cx.cE.a!.bw.dm.ba.a*.a
+    ).bk.c@.an.bH.bJ.cy.c$.ci.bl.b&.az.bD.ak.b8.aK.cL.ah.aa.aa.a%.aa.c
+    e.bk.db.ad.aq.bW.ba.cl.ba.ac.c).b4.aE.cN.b&.bi.cX.bd.aG.ag.b!.bV.b
+    $.a^.a7.c9.ck.av.bE.ci.c^.a^.aM.co.a&.bf.ch.b%.aN.dI.cK.dC.ao.ae.d
+    G.b7.bq.ck.cX.bN.cd.cq.b2.as.ci.aG.bN.db.dr.b5.dk.bR.df.at.b5.aF.a
+    ).aX.bn.dy.bN.ck.a(.c!.cN.ch.dl.bm.b%.a*.aF.dG.b&.c).bp.a#.bs.aF.d
+    q.dL.a&.be.bE.aB.cp.cc.dK.aG.cm.c8.ag.c*.do.cG.bU.ay.a7.dF.al.cf.c
+    &.c3.aB.ct.c$.cy.b4.c4.as.bz.cx.b8.cI.da.dc.av.cI.an.aH.c#.aE.dI.b
+    1.c&.an.bt.bG.b3.a*.cy.dx.bj.dG.cL.cZ.dF.c^.aA.b9.af.ae.cD.c).cb.b
+    P.a(.aP.c6.aK.dn.a5.b2.c2.ds.bY.aa.cV.aU.a9.dx.cY.dg.cR.dc.a$.a5.a
+    1.b9.a#.dC.ag.aK.a*.a).a3.aT.c).ad.dq.cN.a6.bQ.cj.b(.aM.aQ.c!.cS.c
+    K.aB.bb.cy.dk.aX.cG.bl.ae.bG.bA.di.ag.bu.co.bW.aH.aP.an.ab.ao.a2.d
+    q.ap.cC.de.cO.a!.b5.b^.dl.ad.ab.bF.aT.dk""";k = k[ :31] + k[32: ];
+    j=k.index;joi=''.join;c=c.translate(None, '-\n').split('.');#Day9#
+    print(d(joi([chr(j(i[0])*72+j(i[1])) for i in c])))###YSPA Blog###
---
+++ photos and stuff/new_blog_script.py
@@ -0,0 +1,54 @@
+a = """c=\"\"\"aO.cL.aV.dL.bC.cc.ae.dv.ay.aa.aw.dE.b#.aG.aq.at.dd.aa.dF.bi.cg.da.cj.cX.a).c&.cn.a&.aN.bL.c).bt.dw.dJ.av.af.b2.bG.c).b(.b%.aa.aa.a#.aa.di.b1.aa.b0.aa.b9.aa.b^.bM.dB.bL.dI.bg.bC.de.do.dc.a).b(.bl.bh.cC.cz.bR.bS.dM.bq.c8.bu.dr.b^.cD.de.aF.b!.b1.aR.aH.cZ.a#.aC.bz.c*.c6.cq.cY.dm.du.bs.aJ.bC.am.bl.dg.aU.a).b*.c1.co.ao.c*.a9.cz.a2.bx.cr.cv.cm.a@.cO.at.bl.bJ.dM.cY.ae.af.c2.c^.bI.bO.a7.c).bj.dD.at.dE.a6.dp.c!.aA.b0.b1.bT.aQ.b(.a8.aO.bP.db.bU.a*.b).cm.c4.c@.bT.cT.b#.b2.bh.cx.dM.bu.c%.dM.cE.a7.cA.ak.aa.b4.ce.bJ.aF.dg.b@.dJ.bO.bm.av.ab.aj.c@.cv.c1.bE.b0.c!.dq.aQ.b3.bU.cL.b*.bu.dp.c2.aA.dk.cw.c8.aP.cP.bo.cf.aR.cA.dd.aS.cz.du.cS.c&.at.cw.dA.a6.b@.aJ.dd.bO.b8.bz.b@.dE.ck.bR.cf.a$.ck.a1.cK.au.bD.cv.dh.aX.br.bQ.dr.bQ.a!.cx.bh.dD.aD.be.b*.aZ.bU.df.cc.a@.bY.dL.a5.cU.do.a5.bN.ck.dw.bQ.dL.b@.bX.ds.bn.dk.ba.cE.af.at.bM.dm.aZ.c4.cc.c1.as.do.aw.dz.aF.cD.cg.cf.dx.bZ.cs.ds.bJ.bw.c8.dF.a*.b9.ao.cl.bZ.cL.cX.a9.aZ.bM.c6.aD.bf.bS.ab.aa.b4.aq.dC.bY.a%.do.c1.aC.bA.aa.av.aW.cw.bZ.bE.bo.cd.ct.b$.at.aw.aO.du.au.a3.cK.cA.cq.c^.cK.b!.aJ.bd.cE.c0.cE.cr.a4.cF.bn.am.bh.cc.c3.aA.dv.cW.bV.c^.dm.b(.dJ.bE.bK.bd.bx.aU.c0.cx.dK.as.cl.ax.bZ.bq.cA.c2.dj.aM.cR.bW.dn.bk.cE.cD.cO.dv.ap.aq.bk.a2.bD.c*.c7.bz.c9.aT.bb.b(.cL.b!.bj.c0.b5.b(.cA.a@.au.c6.c2.aw.aE.cq.dy.du.aE.b7.cB.ak.aM.c3.cU.dC.cw.b).dk.aJ.bJ.au.cl.a@.bX.c8.aD.bn.ab.ap.aa.ai.cs.c0.cL.da.dm.dz.aH.bJ.b2.bN.c9.dB.a5.av.a).am.ao.am.ao.cG.dH.aw.cL.a#.bI.bY.a%.bR.c7.bW.aE.a6.c0.aF.dt.bz.du.bQ.aP.c6.cY.ar.c2.bu.am.bV.a$.b(.cL.a0.c^.ce.ax.bP.cA.ax.aB.c2.dC.b8.b*.cA.cK.ci.a3.bx.b7.aA.cN.cj.dx.c@.aE.bM.a6.a6.bW.c$.a8.cT.cM.aP.bh.db.bu.bR.cM.bi.bA.bR.a*.cd.bC.dv.cr.a%.cK.ce.dl.ca.ax.bl.bs.aC.br.bm.cD.bX.c).bo.cg.dk.cx.cE.a!.bw.dm.ba.a*.a).bk.c@.an.bH.bJ.cy.c$.ci.bl.b&.az.bD.ak.b8.aK.cL.ah.aa.aa.a%.aa.ce.bk.db.ad.aq.bW.ba.cl.ba.ac.c).b4.aE.cN.b&.bi.cX.bd.aG.ag.b!.bV.b$.a^.a7.c9.ck.av.bE.ci.c^.a^.aM.co.a&.bf.ch.b%.aN.dI.cK.dC.ao.ae.dG.b7.bq.ck.cX.bN.cd.cq.b2.as.ci.aG.bN.db.dr.b5.dk.bR.df.at.b5.aF.a).aX.bn.dy.bN.ck.a(.c!.cN.ch.dl.bm.b%.a*.aF.dG.b&.c).bp.a#.bs.aF.dq.dL.a&.be.bE.aB.cp.cc.dK.aG.cm.c8.ag.c*.do.cG.bU.ay.a7.dF.al.cf.c&.c3.aB.ct.c$.cy.b4.c4.as.bz.cx.b8.cI.da.dc.av.cI.an.aH.c#.aE.dI.b1.c&.an.bt.bG.b3.a*.cy.dx.bj.dG.cL.cZ.dF.c^.aA.b9.af.ae.cD.c).cb.bP.a(.aP.c6.aK.dn.a5.b2.c2.ds.bY.aa.cV.aU.a9.dx.cY.dg.cR.dc.a$.a5.a1.b9.a#.dC.ag.aK.a*.a).a3.aT.c).ad.dq.cN.a6.bQ.cj.b(.aM.aQ.c!.cS.cK.aB.bb.cy.dk.aX.cG.bl.ae.bG.bA.di.ag.bu.co.bW.aH.aP.an.ab.ao.a2.dq.ap.cC.de.cO.a!.b5.b^.dl.ad.ab.bF.aT.dk\"\"\""""
+
+
+a = a.replace(" ", '')
+print(len(a))
+to_replace = """             Y88b   d88P .d8888b. 8888888b.     d8888
+              Y88b d88P d88P  Y88b888   Y88b   d88888
+               Y88o88P  Y88b.     888    888  d88P888
+                Y888P    "Y888b.  888   d88P d88P 888
+                 888        "Y88b.8888888P" d88P  888
+                 888          "888888      d88P   888
+                 888    Y88b  d88P888     d8888888888
+                 888     "Y8888P" 888    d88P     888
+"""
+
+
+
+new = 'import zstd;d=zstd.decompress;k="""abcdefghijklmnopqrstuvwxyzABCDE\nFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()""";'
+print(len('import zstd as z;d=zstd.decompress;k="""abcdefghijklmnopq'))
+c = 0
+for i in range (17):
+    if i == 0:
+        new += a[c : c + 66 - len('FGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()""";')] + '\n'
+        c += 66 - len('FGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()""";')
+    else:
+        new += a[c:c+ 66] + '\n'
+        c += 66
+
+for i in range (len(to_replace)):
+    if to_replace[i] == ' ':
+            new += a[c]
+            c += 1
+    elif to_replace[i] == '\n':
+            new += a[c : c + 13] + '\n'
+            c += 13
+    else:
+            new += "-"
+
+for i in range (17):
+    new += a[c : c + 66]
+    if (i != 16):
+        new += '\n'
+    c += 66
+
+new = new[:-1]
+new += ";k = k[ :31] + k[32: ];\nj=k.index;joi=''.join;c=c.translate(None, '-\\n').split('.');#Day8#\nprint(d(joi([chr(j(i[0])*72+j(i[1])) for i in c])))###YSPA Blog###"
+
+##################The Great YSPA Grind#################
+
+#print(c)
+print(new)
+
+
+#print(a)
